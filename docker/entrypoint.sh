dec8b7e0ad17de2cf3d9a290e5896a9ceeebb191
dejarn <a.jacquelot+1@gmail.com>
2026-05-08 01:07:25 +0200

    fix(docker): pass DATABASE_URL explicitly to prisma migrate deploy

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
-(cd /app/prisma-modules && node node_modules/prisma/build/index.js migrate deploy --schema=/app/prisma/schema.prisma)
+(cd /app/prisma-modules && node node_modules/prisma/build/index.js migrate deploy --schema=/app/prisma/schema.prisma --url="$DATABASE_URL")
 exec node server.js
