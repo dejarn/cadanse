8eb1c472fda0b9d4057a53731302e4615e287575
dejarn <a.jacquelot+1@gmail.com>
2026-05-08 01:22:10 +0200

    fix(docker): copy prisma.config.ts to runner and pass --config to migrate deploy

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
-(cd /app/prisma-modules && node node_modules/prisma/build/index.js migrate deploy --schema=/app/prisma/schema.prisma --url="$DATABASE_URL")
+(cd /app/prisma-modules && node node_modules/prisma/build/index.js migrate deploy --schema=/app/prisma/schema.prisma --config=/app/prisma.config.ts)
 exec node server.js
